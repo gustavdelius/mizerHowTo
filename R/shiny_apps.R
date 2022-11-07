88c3d7751f4f7d72c38c855bd2ac36d5371db1a0
Romain Forestier <romain.forestier48@gmail.com>
2022-11-07 14:14:26 +1100

    improving shiny_erepro

--- R/shiny_apps.R
+++ R/shiny_apps.R
@@ -1,71 +1,97 @@
+#' @title Erepro calibration app
 
-
-#' shiny app to tweak erepro
+#' @description Shiny app which focus on tweaking erepro per species and see the
+#' effects on growth, feeding level, diet and shape of the size spectrum / biomass
+#' through time
+#'
+#' @param params An object of class \linkS4class{MizerParams}.
 #'
 #' @export
-shiny_erepro <- function(input, dat = NULL) {
-  params_shiny <- input
-  ui=fluidPage(
 
-    # Application title
-    titlePanel("erepro calibration"),
+shiny_erepro <- function(params)
+{
 
-    fluidRow(
-      column(4, wellPanel(
-        #sliderInput("kappa", "log10 Resource Carrying Capacity:", min = 8, max = 12, value = log10(params_shiny@resource_params$kappa),
-        #            step = 0.1),
-        #   sliderInput("Rmax", "log10 Maximum Recruitment:", min = 1, max = 12, value = 12,
-        #              step = 0.1),
-        sliderInput("erepro", "log10 Reproductive Efficiency:", min = -8, max = 1, value = log10(params_shiny@species_params$erepro[1]),
-                    step = 0.1)
-      )),
-      column(6,
-             plotOutput("plot1", width = 600, height = 600),
-             if(!is.null(dat)) plotOutput("plot2", width = 600, height = 600)
-      ))
+    params_shiny <- params
 
+    ui=fluidPage(
+        titlePanel("Erepro calibration"),
+        fluidRow(
+            column(2,
+                   wellPanel(
+                       actionButton("run", "Run the simulation")
+                   ),
+                   wellPanel(
+                       actionButton("done", "Return", icon = icon("check"),
+                                    onclick = "setTimeout(function(){window.close();},500);")
+                   ),
+                   wellPanel(
+                       uiOutput("toggle_erepro"))
+            )
+            ,
+            column(5,
+                   plotOutput("plotGrowth", height = 600),
+                   plotOutput("plotFeed", height = 600),
+            ),
+            column(5,
+                   plotOutput("plotDiet", height = 600),
+                   plotOutput("plotCal", height = 600)
+            )
+        )
+    )
 
+    server <- function(input, output) {
+        output$toggle_erepro <- renderUI({
+            n_spec <- length(params_shiny@species_params$species)
+            lapply(1:n_spec, function(i) {
+                div(
+                    paste(params_shiny@species_params$species[i],
+                          'Initial Value:',
+                          params_shiny@species_params$erepro[i]),
+                    numericInput(inputId = paste0('erepro', i),
+                                 "erepro:",
+                                 value = params_shiny@species_params$erepro[i])
+                )
+            })
+        })
+        # reactive expression
+        sim <- eventReactive(input$run, {
+            n_spec <- length(params_shiny@species_params$species)
+            new_erepro <- c(sapply(1:n_spec, function(i) {
+                inputName <- paste("erepro", i, sep = "")
+                input[[inputName]]
+            }))
 
-  )
-  server = function(input, output) {
+            shiny_erepro_output <<- new_erepro
 
-#    sim <- observeEvent(input$erepro, {
-#       print("yo")
-#       params_shiny@species_params$erepro <- rep(10^input$erepro,12)
-#       params_shiny <- setParams(params_shiny)
-#       sim_shiny <- project(params_shiny, effort = 1, t_max = 50)
-# })
-#     output$plot1 <- renderPlot({
-#       plot(sim())
-#     })
+            params_shiny@species_params$erepro <- new_erepro
+            params_shiny <- setParams(params_shiny)
+            sim_shiny <- project(params_shiny, effort = 1, t_max = 100)
+        })
 
-    output$plot1 <- renderPlot({
-      # set up params using values given, need check and change parameter values so units work in days units
-      params_shiny@species_params$erepro <- rep(10^input$erepro,length(params_shiny@species_params$erepro))
-      # params@species_params$Rmax <- rep(10^input$Rmax,12)
-      params_shiny <- setParams(params_shiny)#,kappa=10^input$kappa)
-      # run without fishing
-      sim_shiny <- project(params_shiny, effort = 1, t_max = 50)
-      plot(sim_shiny)
-    })
+        output$plotGrowth <- renderPlot({
+            plotGrowthCurves2(sim(), species_panel = T)
+        })
 
-    if(!is.null(dat))
-    {
-    output$plot2 <- renderPlot({
-      # set up params using values given, need check and change parameter values so units work in days units
-      params_shiny@species_params$erepro <- rep(10^input$erepro,length(params_shiny@species_params$erepro))
-      # params@species_params$Rmax <- rep(10^input$Rmax,12)
-      params_shiny <- setParams(params_shiny)#,kappa=10^input$kappa)
-      # run without fishing
-      sim_shiny <- project(params_shiny, effort = 1, t_max = 100)
-      plotPredObsYield(sim_shiny,dat)
-      # plotBiomass(sim_shiny)
-    })
+        output$plotFeed <- renderPlot({
+            plotFeedingLevel(sim(), include_critical = T)
+        })
+
+        output$plotCal <- renderPlot({
+            plotCalibration(sim())
+        })
+
+        output$plotDiet <- renderPlot({
+            plotDiet2(sim())
+        })
+
+        observeEvent(input$done, {
+            stopApp(sim()@params@species_params$erepro)
+        })
+
     }
-  }
-  shinyApp(ui, server)
-}
 
+    runGadget(ui, server, viewer = browserViewer())
+}
 
 
 #' shiny app to tweak erepro per species and see the effects on Fmsy
